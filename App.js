0bc7076750e1ab98d49227041dccccad9235d43e
VikramSingamsetti <vikram.singams@gmail.com>
2025-11-24 15:55:01 +0530

    Episode 2 and 3

--- App.js
+++ App.js
@@ -1,26 +1,49 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1>Im h1 tag</h1>
- *           <h2>Im h2 tag</h1>
- *      </div>
- * </div>
- * 
- * 
- * 
- * 
- */
-
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", { id: "h1" }, "Hello H1"),
-        React.createElement("h2", { id: "h2" }, "Hello H2")]))
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
+// This very hard to read so Facebook developers introduced JSX
+//React.createElement => Object => HtmlElement(render)
+const heading = React.createElement("h1", { id: "heading" }, "Hello Namste React"); 
 
 
-//const heading = React.createElement("h1", { id: "heading" }, "Hello react");
+//JSx Multiline
+const jsxHeadingMultiline= (<div><h1>
+    Hello Namsthe React JSX 
+    </h1></div>);
+
+    // Functional Component
+    const HeadingComponent1 = () => (
+        <h1>Hello Functional Component1</h1>
+    )
+
+     // Functional Component
+    const HeadingComponent3 = () => {
+       return <h1>Hello Functional Component3</h1>
+    }
+
+     // Functional Component
+    const HeadingComponent4 = () => <h1>Hello Functional Component4</h1>;
+    //Component Composition
+    const ComponentComposition = ()=>(
+        <div>
+            {100+300}
+            <h1>Component Composition</h1>
+            <HeadingComponent4/>
+        </div>
+    )
+// This is jsx code, but finally in coverts into React.createElement, But it is human friendly
+const jsxHeading= (
+    <div>
+        {100+300}
+         <HeadingComponent1/>
+        <HeadingComponent3/>
+<h1>Hello Namsthe React JSX1 </h1>
+    </div>
+) 
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(jsxHeading); // This is to render element
 
-root.render(parent); 
+root.render(<ComponentComposition/>); // This is to render Component
